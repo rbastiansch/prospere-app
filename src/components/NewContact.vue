bcb4b53f020b471b18cc6812fdcdc42cd227af03
Rafael Bastiansch <rafaelbastiansch@gmail.com>
2017-12-18 16:53:07 -0200

    add vee-validate on new contact form

--- src/components/NewContact.vue
+++ src/components/NewContact.vue
@@ -2,12 +2,13 @@
   <div v-if="hasToken" class="new-contact">
     <Navigation />
     <h2>New Contact</h2>
-    <form class="form">
+    <form class="form" v-on:submit.prevent="validateBeforeSubmit">
       <div class="control-input">
-        <input class="input" v-model="name" type="text" name="name" placeholder="Name">
+        <input v-validate="'required|alpha|min:3'" class="input" v-model="name" type="text" name="name" placeholder="Name">
+        <span v-show="errors.has('first_name')">{{ errors.first('first_name') }}</span>
       </div>
       <div class="control-input">
-        <input class="input" v-model="address" type="text" name="email" placeholder="Email">
+        <input v-validate="'required|email'" class="input" v-model="address" type="text" name="email" placeholder="Email">
       </div>
       <div class="control-input">
         <input class="input" v-model="phone" type="text" name="phone" placeholder="Phone">
@@ -15,17 +16,24 @@
       <div class="control-textarea">
         <textarea v-model="background" name="about" rows="5" cols="46" placeholder="About"></textarea>
       </div>
+      <div class="control-errors">
+        <span v-if="submitted" v-show="errors.has('email')">{{ errors.first('email') }}</span>
+        <span v-if="submitted" v-show="errors.has('name')">{{ errors.first('name') }}</span>
+      </div>
       <div class="control-button">
-        <button class="button" type="button" name="submit" v-on:click="addContact">Submit</button>
+        <button class="button" type="submit" name="submit">Submit</button>
       </div>
     </form>
   </div>  
 </template>
 
 <script>
+import Vue from 'vue';
+import VeeValidate from 'vee-validate';
 import Navigation from '@/components/Navigation';
 import { addContact } from '@/services/contacts';
 
+Vue.use(VeeValidate);
 export default {
   name: 'NewContact',
   components: {
@@ -38,6 +46,7 @@ export default {
       phone: '',
       background: '',
       hasToken: '',
+      submitted: false,
     };
   },
   computed: {
@@ -59,6 +68,16 @@ export default {
     }
   },
   methods: {
+    validateBeforeSubmit() {
+      this.$validator.validateAll()
+        .then((result) => {
+          if (result) {
+            this.addContact();
+            return;
+          }
+          this.submitted = true;
+        });
+    },
     addContact() {
       const { token, email, name, phone, address, background } = this;
       const code = phone.substring(0, 2);
